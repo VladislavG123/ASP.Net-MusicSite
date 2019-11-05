e13465053b1e57527d1bcbf95bb29e5b7fd81768
Владислав Горохов <gbg.0101@mail.ru>
2019-11-05 18:13:24 +0600

    first commit

---
+++ MusicSite/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DownloadMusic(int musicId)
+        {
+            switch (musicId)
+            {
+                case 0:
+                    return File(@"~\Content\files\bensound-betterdays.mp3", "audio/mpeg");
+                case 1:
+                    return File(@"~\Content\files\bensound-dubstep.mp3", "audio/mpeg");
+                case 2:
+                    return File(@"~\Content\files\bensound-sunny.mp3", "audio/mpeg");
+                default:
+                    return new HttpStatusCodeResult(500);
+            }
+        }
+        public ActionResult DownloadAlbum()
+        {
+            return File(@"~\Content\files\album.zip", "application/zip");
+        }
+    }
+}
